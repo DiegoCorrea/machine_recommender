6a765f098aca59a69162e073b180ef0edadba2b2
Diego Corrêa <diegocorrea.cc@gmail.com>
2019-03-19 17:20:39 -0300

    minerando dados estavel

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ from src.data_mining import DataMining
 
 if __name__ == '__main__':
     # Create Dataset
+    print("*"*50)
     print("1.\tCriando os conjuntos de dados")
-    DataMining.create()
+    print("-" * 50)
+    # DataMining.create()
+    print("*" * 50)
     # Load Dataset
+    print("*" * 50)
     print("2.\tCarregando os dados limpos")
+    print("-" * 50)
     SONGSET = DataMining.load_song_set()
     SONGSET.info(memory_usage='deep')
+    print(str(SONGSET.song_id.nunique()))
     print("\n")
     USERSET = DataMining.load_user_set()
     USERSET.info(memory_usage='deep')
-    print("2.\tPre Processamento")
+    print(str(USERSET.user_id.nunique()))
+    print("*" * 50)
+    print("3.\tPre Processamento")
+    print("-" * 50)
     # DATASET = preprocessing.main_start(SONGSET)
     # DATASET.info(memory_usage='deep')
     # print("\n")
--- src/data_mining.py
+++ src/data_mining.py
@@ -15,29 +15,31 @@ class DataMining:
         gender_df = pd.read_csv(DataMining.raw_data_path + 'msd-MAGD-genreAssignment.cls',
                                 sep='\t', names=['track_id', 'gender'])
         song_msd_df = pd.read_csv(DataMining.raw_data_path + 'songs.csv',
-                                  names=['id', 'title', 'album', 'artist', 'year'])
+                                  names=['song_id', 'title', 'album', 'artist', 'year'])
         song_by_track_df = pd.read_csv(DataMining.raw_data_path + 'unique_tracks.txt',
-                                       sep='<SEP>', names=['track_id', 'id', 'title', 'artist'])
+                                       sep='<SEP>', names=['track_id', 'song_id', 'title', 'artist'])
         to_merge_df = song_by_track_df.drop(['title', 'artist'], axis=1)
-        song_msd_df = song_msd_df.drop_duplicates(['id'])
+        song_msd_df = song_msd_df.drop_duplicates(['song_id'])
+        song_msd_df.info(memory_usage='deep')
         # Join
-        self.__song_df = pd.merge(pd.merge(song_msd_df, to_merge_df, how='left', left_on='id', right_on='id'),
+        self.__song_df = pd.merge(pd.merge(song_msd_df, to_merge_df, how='left', left_on='song_id', right_on='song_id'),
                                   gender_df, how='inner', left_on='track_id', right_on='track_id')
+        self.__song_df = self.__song_df.drop_duplicates(['song_id'])
 
     def filter_data_users_by_songs(self):
         users_preferences_df = pd.read_csv(
             DataMining.raw_data_path + 'train_triplets.txt',
             sep='\t', names=['user_id', 'song_id', 'play_count']
         )
-        users_preferences_df.info(verbose=True)
+        users_preferences_df.info(memory_usage='deep')
+        print(str(users_preferences_df.user_id.nunique()))
         self.__users_preferences_df = users_preferences_df[
-            users_preferences_df['song_id'].isin(self.__song_df['id'].tolist())
+            users_preferences_df['song_id'].isin(self.__song_df['song_id'].tolist())
         ]
-        self.__users_preferences_df.info(verbose=True)
 
     def save(self):
         self.__song_df.to_csv(DataMining.clean_data_path + 'songs.csv', index=False,
-                              columns=['id', 'title', 'artist', 'album', 'gender'])
+                              columns=['track_id', 'song_id', 'title', 'artist', 'album', 'gender'])
         self.__users_preferences_df.to_csv(DataMining.clean_data_path + 'play_count.csv', index=False,
                                            columns=['user_id', 'song_id', 'play_count'])
 
@@ -57,7 +59,7 @@ class DataMining:
     @staticmethod
     def load_song_set():
         return pd.read_csv(DataMining.clean_data_path + 'songs.csv',
-                           names=['id', 'title', 'album', 'artist', 'gender'])
+                           names=['song_id', 'title', 'album', 'artist', 'gender'])
 
     @staticmethod
     def load_user_set():

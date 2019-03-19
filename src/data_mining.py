666762a8e7f8ff74e4895c1889770fa12bff85d1
Diego Correa <diegocorrea.cc@gmail.com>
2019-03-19 09:32:03 -0300

    Trocando o nome de Preprocessing para Data Mining e refatorando os dados

--- src/data_mining.py
+++ src/data_mining.py
@@ -3,19 +3,19 @@ import os
 import pandas as pd
 
 
-class PreProcessing:
+class DataMining:
     def __init__(self):
-        self.__raw_data_path = "/data/original_set/"
-        self.__clean_data_path = "/data/clean_set/"
+        self.__raw_data_path = os.getcwd() + "/data/original_set/"
+        self.__clean_data_path = os.getcwd() + "/data/clean_set/"
         self.__song_df = None
         self.__users_preferences_df = None
 
-    def link_songs(self):
-        gender_df = pd.read_csv(os.getcwd() + '/data/original_set/msd-MAGD-genreAssignment.cls',
+    def linked_songs(self):
+        gender_df = pd.read_csv(self.__raw_data_path + 'msd-MAGD-genreAssignment.cls',
                                 sep='\t', names=['track_id', 'gender'])
-        song_msd_df = pd.read_csv(os.getcwd() + '/data/original_set/songs.csv',
+        song_msd_df = pd.read_csv(self.__raw_data_path + 'songs.csv',
                                   names=['id', 'title', 'album', 'artist', 'year'])
-        song_by_track_df = pd.read_csv(os.getcwd() + '/data/original_set/unique_tracks.txt',
+        song_by_track_df = pd.read_csv(self.__raw_data_path + 'unique_tracks.txt',
                                        sep='<SEP>', names=['track_id', 'id', 'title', 'artist'])
         to_merge_df = song_by_track_df.drop(['title', 'artist'], axis=1)
         song_msd_df = song_msd_df.drop_duplicates(['id'])
@@ -23,9 +23,9 @@ class PreProcessing:
         self.__song_df = pd.merge(pd.merge(song_msd_df, to_merge_df, how='left', left_on='id', right_on='id'),
                                   gender_df, how='inner', left_on='track_id', right_on='track_id')
 
-    def link_user_and_songs(self):
+    def filter_data_users_by_songs(self):
         users_preferences_df = pd.read_csv(
-            os.getcwd() + '/data/original_set/train_triplets.txt',
+            self.__raw_data_path + 'train_triplets.txt',
             sep='\t', names=['user_id', 'song_id', 'play_count']
         )
         users_preferences_df.info(verbose=True)
@@ -35,9 +35,9 @@ class PreProcessing:
         self.__users_preferences_df.info(verbose=True)
 
     def save(self):
-        self.__song_df.to_csv(os.getcwd() + self.__clean_data_path + 'songs.csv', index=False,
+        self.__song_df.to_csv(self.__clean_data_path + 'songs.csv', index=False,
                               columns=['id', 'title', 'artist', 'album', 'gender'])
-        self.__users_preferences_df.to_csv(os.getcwd() + self.__clean_data_path + 'play_count.csv', index=False,
+        self.__users_preferences_df.to_csv(self.__clean_data_path + 'play_count.csv', index=False,
                                            columns=['user_id', 'song_id', 'play_count'])
 
     def get_song_df(self):

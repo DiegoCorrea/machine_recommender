19dfaa226cd358e1912a97a21787354dde741ae8
Diego Corrêa <diegocorrea.cc@gmail.com>
2019-04-16 19:09:14 -0300

    finalizando a implementação do KNN basico

--- src/algorithms/knn.py
+++ src/algorithms/knn.py
@@ -1,14 +1,15 @@
 import math
 from multiprocessing.dummy import Pool as ThreadPool
 import functools
+from statistics import mode
 from src.globalVariable import GlobalVariable
 
 
 class KNN:
     def __init__(self, dataset, label, n_neighbors=3):
-        self.__X_TRAIN = dataset
-        self.__Y_TRAIN = label
-        self.__n_neighbors = n_neighbors
+        self.__DATASET_TRAIN = dataset
+        self.__LABELS_TRAIN = label
+        self.__N_NEIGHBORS = n_neighbors
 
     @staticmethod
     def __euclidean_distance(x_test, x_train):
@@ -17,19 +18,31 @@ class KNN:
     def __neigh(self, x_test):
         pool = ThreadPool(GlobalVariable.processor_number)
         result = pool.map(functools.partial(KNN.__euclidean_distance, tuple(x_test.values.tolist())),
-                          self.__X_TRAIN.values.tolist())
+                          self.__DATASET_TRAIN.values.tolist())
         pool.close()
         pool.join()
         return result
 
-    def predict(self, x_test):
-        print(x_test.values.tolist())
-        index_list = self.__X_TRAIN.index.values
-        # for index, row in x_test.iterrows():
-        test_neigh = self.__neigh(x_test)
-        list__ = {a: b for a, b in zip(index_list, test_neigh)}
-        s = [(k, list__[k]) for k in sorted(list__, key=list__.get, reverse=False)]
-        print(s[:3])
+    def predict_k(self, dataset_test):
+        index_list = self.__DATASET_TRAIN.index.values
+        all_neigh = []
+        for index, row in dataset_test.iterrows():
+            test_neigh = self.__neigh(row)
+            list__ = {a: b for a, b in zip(index_list, test_neigh)}
+            ordered_neigh = [(k, list__[k]) for k in sorted(list__, key=list__.get, reverse=False)]
+            all_neigh.append({index: ordered_neigh[:self.__N_NEIGHBORS]})
+        return all_neigh
+
+    def predict(self, dataset_test):
+        index_list = self.__DATASET_TRAIN.index.values
+        all_neigh = []
+        for index, row in dataset_test.iterrows():
+            test_neigh = self.__neigh(row)
+            list__ = {a: b for a, b in zip(index_list, test_neigh)}
+            ordered_neigh = [(k, list__[k]) for k in sorted(list__, key=list__.get, reverse=False)]
+            label_test = [self.__LABELS_TRAIN[i] for i, v in ordered_neigh[:self.__N_NEIGHBORS]]
+            all_neigh.append({index: mode(label_test)})
+        return all_neigh
 
     @staticmethod
     def test():
@@ -37,6 +50,6 @@ class KNN:
         import pandas as pd
         data = load_iris()
         df = pd.DataFrame(data.data, columns=data.feature_names)
-        knn = KNN(dataset=df.iloc[1:149], label=data['target'])
-        knn.predict(df.iloc[0])
-        return knn
+        knn = KNN(dataset=df[3:149], label=data['target'])
+        predict = knn.predict(df[0:2])
+        print(predict)

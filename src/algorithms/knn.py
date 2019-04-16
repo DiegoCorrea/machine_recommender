78e05fc73f5a723cea810481d5004c54c02ab019
Diego Correa <diegocorrea.cc@gmail.com>
2019-04-16 10:17:25 -0300

    KNN funcionando

--- src/algorithms/knn.py
+++ src/algorithms/knn.py
@@ -1,26 +1,42 @@
 import math
 from multiprocessing.dummy import Pool as ThreadPool
 import functools
+from src.globalVariable import GlobalVariable
 
 
 class KNN:
-    def __init__(self, x_train, y_train, n_neighbors=3, n_jobs=3):
-        self.__X_TRAIN = x_train
-        self.__Y_TRAIN = y_train
+    def __init__(self, dataset, label, n_neighbors=3):
+        self.__X_TRAIN = dataset
+        self.__Y_TRAIN = label
         self.__n_neighbors = n_neighbors
-        self.__n_jobs = n_jobs
 
     @staticmethod
     def __euclidean_distance(x_test, x_train):
-        return math.sqrt(sum([(a - b) ** 2 for a, b in zip(x_test.values.tolist(), x_train.values.tolist())]))
+        return math.sqrt(sum([(a - b) ** 2 for a, b in zip(x_test, x_train)]))
 
     def __neigh(self, x_test):
-        pool = ThreadPool(self.__n_jobs)
-        result = pool.map(functools.partial(KNN.__euclidean_distance, dimension_x=x_test), self.__X_TRAIN)
+        pool = ThreadPool(GlobalVariable.processor_number)
+        result = pool.map(functools.partial(KNN.__euclidean_distance, tuple(x_test.values.tolist())),
+                          self.__X_TRAIN.values.tolist())
         pool.close()
         pool.join()
+        return result
 
     def predict(self, x_test):
-        for index, row in x_test.iterrows():
-            self.__neigh(row)
+        print(x_test.values.tolist())
+        index_list = self.__X_TRAIN.index.values
+        # for index, row in x_test.iterrows():
+        test_neigh = self.__neigh(x_test)
+        list__ = {a: b for a, b in zip(index_list, test_neigh)}
+        s = [(k, list__[k]) for k in sorted(list__, key=list__.get, reverse=False)]
+        print(s[:3])
 
+    @staticmethod
+    def test():
+        from sklearn.datasets import load_iris
+        import pandas as pd
+        data = load_iris()
+        df = pd.DataFrame(data.data, columns=data.feature_names)
+        knn = KNN(dataset=df.iloc[1:149], label=data['target'])
+        knn.predict(df.iloc[0])
+        return knn
